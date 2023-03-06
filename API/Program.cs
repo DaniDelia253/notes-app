3575b19354d069300d5992920e40c4871f4d0a04
DaniDela253 <danidavis321@gmail.com>
2023-03-06 13:36:14 -0600

    add dev env token key and resolve swagger error

--- API/Program.cs
+++ API/Program.cs
@@ -15,11 +15,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
